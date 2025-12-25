1f6ac2a5a66fdfe4cc29d89e04cb294589c37201
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2025-12-26 00:08:50 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js';
 
 dotenv.config({
     path: './env'
@@ -13,7 +14,7 @@ connectDB()
 // Promise
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
     })
 
     app.on("error", (error) => {
@@ -26,6 +27,38 @@ connectDB()
     console.log("MONGO db connection failed !!! ", error);
 })
 
+
+
+
+/*
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+const app = express(); // ✅ app defined
+
+// middlewares
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+    res.send("API is running 🚀");
+});
+
+connectDB()
+    .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT || 8000}`);
+    });
+    })
+    .catch((error) => {
+    console.log("MONGO db connection failed !!! ", error);
+    });
+*/
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,8 @@
-import { Promise } from "mongoose"
-
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch(err => next(err))
     }
 }
-
-
-
 
 
 export {asyncHandler}
